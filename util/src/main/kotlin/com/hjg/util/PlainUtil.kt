2b211cdf0bcfa23bc02783249ee6872ff7767164
ccka <computer_cka@163.com>
2025-06-18 19:09:10 +0800

    正确处理时区

--- util/src/main/kotlin/com/hjg/util/PlainUtil.kt
+++ util/src/main/kotlin/com/hjg/util/PlainUtil.kt
@@ -7,9 +7,8 @@ import java.util.*
 class PlainUtil {
     //如果kotlin-springboot-demo中的代码，引用这个字段，编译会报错.
     //kotlin-springboot-demo中引用的地方，会报找不到这个class。
-    val dateformat = FastDateFormat.getInstance("yyyy-MM-dd",
-        TimeZone.getTimeZone("GMT+:08:00"),
-        Locale.CHINA)
+    val dateformat = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss",
+        TimeZone.getTimeZone("GMT+8:00"))
 
     fun trimString(string: String) : String {
         return StringUtils.trimToEmpty(string)
