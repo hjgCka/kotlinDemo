c33d264fd6e4b3a9381849c29058564532a6764c
hjg <computer_cka@163.com>
2026-06-18 19:39:40 +0800

    更新注释。

--- kotlin-springboot-demo/build.gradle.kts
+++ kotlin-springboot-demo/build.gradle.kts
@@ -22,20 +22,27 @@ plugins {
     id("shared-std-kotlin")
 
     /**
-     * kotlin官方维护的插件，如果不加上它，总是会提醒@SpringBootApplication注解的类
-     * 需要被设为open。
-     * 它与@Configuration的proxyBeanMethods属性有关系。
+     * kotlin官方维护的插件。
+     * kotlin-plugin.spring：解决 Kotlin 默认 final 不能被 CGLIB 继承代理的语法问题，作用在编译期；
+     * proxyBeanMethods：控制 Spring 是否要对 @Configuration 类做 CGLIB 代理的业务特性开关，作用在 Spring 容器初始化阶段；
+     * 只要你的配置类用默认 proxyBeanMethods=true，必须依赖该插件；关闭代理后只是绕开了配置类的限制，但业务层 AOP 依旧需要这个插件。
+     *
+     * 最佳实践：
+     * 1. Kotlin + Spring Boot 项目永远建议带上 kotlin("plugin.spring")；
+     * 2. 配置类统一使用 @Configuration(proxyBeanMethods = false) 优化启动性能，规避配置类内部 @Bean 方法直接调用的坑；
+     * 3. 插件依然保留，用来保证事务、缓存、异步等 AOP 功能正常代理生效。
      */
     //kotlin("plugin.spring")
-    id("org.jetbrains.kotlin.plugin.spring") version "2.1.20"
+    id("org.jetbrains.kotlin.plugin.spring") version "2.3.20"
 
     id("org.springframework.boot") version "3.4.6"
 }
 
+// 版本与由org.springframework插件引入的插件版本绑定
 apply(plugin = "io.spring.dependency-management")
 
 dependencies {
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation(project(":util"))
